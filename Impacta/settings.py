a7517476e1befb5f44db9404fad9cc8b5e4eedde
lucasgsouza03 <30882839+lucasgsouza03@users.noreply.github.com>
2017-11-08 22:26:38 -0200

    Update settings.py

--- Impacta/settings.py
+++ Impacta/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Impacta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': mysqlconndict['Database'],
+        'USER': mysqlconndict['User Id'],
+        'PASSWORD': mysqlconndict['Password'],
+        'HOST': mysqlconndict['Data Source'].split(':')[0],
+        'PORT': mysqlconndict['Data Source'].split(':')[1],
     }
 }
 
