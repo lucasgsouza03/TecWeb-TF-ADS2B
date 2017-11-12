fafaacc78047ad7359f7bb98cc9ff09127c91b37
OziasSoares <30847713+OziasSoares@users.noreply.github.com>
2017-11-12 20:24:24 -0200

    Update models.py

--- core/models.py
+++ core/models.py
@@ -46,11 +46,14 @@ class Usuario(AbstractBaseUser):
     def get_full_name(self):
         return self.nome
 
+    def __str__(self):
+        return self.nome
+
 class Curso(models.Model):
     sigla = models.CharField(max_length=5)
     nome = models.CharField(max_length=100)
     tipo = models.CharField(max_length=50,blank=True)
-    carga_hoaria = models.IntegerField(default=1000)
+    carga_horaria = models.IntegerField(default=1000)
     ativo = models.BooleanField(default=True)
 
     descricao = models.TextField(blank=True)
