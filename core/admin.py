f7d62f3eaec8251a8ab68224c2c417cb4bb1a387
Mael07 <30848000+Mael07@users.noreply.github.com>
2017-11-12 19:30:13 -0200

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core.models import Curso, Aluno
+
+class AlunoAdmin(UserAdmin):
+    list_display = ('ra', 'email', 'nome', 'curso')
+    list_filter = ('perfil',)
+    fieldsets = ( (None, {'fields': ('ra', 'email', 'nome', 'curso')}),)
+    add_fieldsets = (
+        (None, { 
+            'fields': ('ra', 'email', 'nome', 'curso', 'password1', 'password2')
+            }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
 
 # Register your models here.
+admin.site.register(Aluno,AlunoAdmin)
+admin.site.register(Curso)
+
