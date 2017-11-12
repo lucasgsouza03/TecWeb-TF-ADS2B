dffc35efd56dd13d057f17ac53daf2f72371dc64
lucasgsouza03 <lucas_mardek@hotmail.com>
2017-11-12 21:09:53 -0200

    ok

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class teste(models.Model):
+class curso(models.Model):
     nome = models.CharField(max_length=200)
     def __str__(self):
-        return self.nome
+        return self.nome        
