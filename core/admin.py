45e077bda92fd417179148b4b0ba58881e80ec83
GersonHashimoto <30847882+GersonHashimoto@users.noreply.github.com>
2017-11-21 22:21:14 -0200

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
 from django.contrib.auth.admin import UserAdmin
-from core.models import Curso, Aluno
+from core.models import Curso, Aluno, Professor
 from django import forms
 
 class NovoAlunoForm(forms.ModelForm):
@@ -40,7 +39,39 @@ class CursoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'tipo', 'carga_horaria')
     list_filter = ('tipo',)
 
+class NovoProfessorForm(forms.ModelForm):
+    class Meta:
+        model = Professor
+        fields = ('nome', 'ra', 'email', 'apelido', 'celular')
+    def save(self, commit=True):
+        user = super(NovoProfessorForm, self).save(commit=False)
+        user.set_password('123@mudar')
+        user.perfil = 'P'
+        if commit:
+            user.save()
+        return user
+
+class AlterarProfessorForm(forms.ModelForm):
+    class Meta:
+        model = Professor
+        fields = ('nome', 'ra', 'email', 'apelido', 'celular')
+
+class ProfessorAdmin(UserAdmin):
+    form = AlterarProfessorForm
+    add_form = NovoProfessorForm
+    list_display = ('nome', 'ra', 'email', 'apelido', 'celular')
+    list_filter = ('perfil',)
+    fieldsets = ( (None, {'fields': ('nome', 'ra', 'email', 'apelido', 'celular')}),)
+    add_fieldsets = (
+        (None, { 
+            'fields': ('nome', 'ra', 'email', 'apelido', 'celular')
+            }),
+    )
+    search_fields = ('apelido',)
+    ordering = ('apelido',)
+    filter_horizontal = ()
+
 # Register your models here.
 admin.site.register(Aluno,AlunoAdmin)
 admin.site.register(Curso,CursoAdmin)
-
+admin.site.register(Professor,ProfessorAdmin)
