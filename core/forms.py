d3049ff041ab2f130039b55c4fe6ae3fc14bc79d
GersonHashimoto <30847882+GersonHashimoto@users.noreply.github.com>
2017-11-28 19:34:35 -0200

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -13,6 +13,17 @@ class contato_forms(forms.Form):
     def envia_email(self, assunto, mensagem, email):
         email = [email]
         send_mail(assunto, mensagem, EMAIL_HOST_USER, email, fail_silently=True)
+    
+class solicita_matricula(forms.Form):
+    nome = forms.CharField()
+    email = forms.EmailField()
+    cel = forms.CharField()
+    
+    def envia_email(self, nome, email, cel, curs):
+        assunto  = "Solicitação de matricula"
+        mensagem = "Informações\nNome: {}\nE-mail: {}\nTelefone: {}\nCurso desejado: {}".format(nome, email, cel, curs)
+        email = ['lucasgdesouza03@gmail.com']
+        send_mail(assunto, mensagem, EMAIL_HOST_USER, email, fail_silently=True)
 
 
 class questao_arquivo(forms.ModelForm):
