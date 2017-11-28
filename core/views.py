769a42084586459c9e6c13b00fbd5963d6045ad5
lucasgsouza03 <30882839+lucasgsouza03@users.noreply.github.com>
2017-11-28 18:02:56 -0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
-from core.forms import contato_forms, questao_arquivo, solicita_matricula
+from core.forms import contato_forms, questao_arquivo
 from core.models import Curso
 
 # Create your views here.
@@ -27,6 +27,7 @@ def cursos(request):
     }
     return render(request, "cursos.html", contexto)
 def detalhes(request, sigla):
+'''	
     if request.POST:
         form = solicita_matricula(request.POST)
         if form.is_valid():
@@ -37,10 +38,11 @@ def detalhes(request, sigla):
             form.envia_email(nome, email, cel, curs)
     else:
         form = contato_forms()
+'''
     curso = Curso.objects.get(sigla=sigla)
     contexto = {
-        "curso": curso,
-        "form": form
+        "curso": curso
+        #"form": form
     }
     return render(request, "detalhes.html", contexto)
 def EsqueciSenha(request):
